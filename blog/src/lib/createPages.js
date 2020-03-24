8a34c1bb34b6dd5cfacc804e9b6ae17f0c4bfc35
DahuinWorld <jeonghye9808@naver.com>
2020-03-24 17:55:28 +0900

    마크다운 파일 블로그 게시글로 표시하기

--- blog/src/lib/createPages.js
+++ blog/src/lib/createPages.js
@@ -2,22 +2,45 @@
 const path = require('path'); //import 대신 const, require을 사용!
 const {CreatePagesArgs} = require('gatsby');
 
-const pages = [
-    {id:1, content:'Gatsby 로 블로그 만들기'},
-    {id:2, content:'하나하나 따라해보기'},
-    {id:3, content:'조금씩 나아가기!'},
-];
+// const pages = [
+//     {id:1, content:'Gatsby 로 블로그 만들기'},
+//     {id:2, content:'하나하나 따라해보기'},
+//     {id:3, content:'조금씩 나아가기!'},
+// ];
 
-async function createPages({actions,CreatePagesArgs}) {
+async function createPages({actions, graphql, CreatePagesArgs}) {
     const { createPage } = actions;
-    pages.forEach(page => {
+
+    const { data, errors } = await graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        html
+                        frontmatter {
+                            title
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    if (errors) {
+        throw errors;
+    }
+
+    data.allMarkdownRemark.edges.forEach(({ node }) => {
         createPage({
-            path: page.id.toString(),
-            context: page,
-            component:path.resolve(__dirname, '../templates/PostTemplate.js'),
+            path : node.frontmatter.title,
+            context : {
+                html : node.html,
+                title : node.frontmatter.title,
+            },
+            component : path.resolve(__dirname, '../templates/PostTemplate.js'),
         });
     });
-    console.log("that's work!!");
 }
+
 
 exports.createPages = createPages;
