9ef49a7c482524acbab740fde4d5a41658a8fbe5
DahuinWorld <jeonghye9808@naver.com>
2020-03-25 22:09:30 +0900

    게시글 목록표시

--- blog/src/templates/PostTemplate.js
+++ blog/src/templates/PostTemplate.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Layout from '../components/layout';
 
 const PostTemplate = React.memo(props => {
+    const { title, date, html } = props.pageContext;
     return (
         <Layout>
-            <code>
-                <pre>{JSON.stringify(props, null, 4)}</pre>
-            </code>
+            <h2>{title}</h2>
+            <h4>{date}</h4>
+            <hr />
+            <div dangerouslySetInnerHTML={{__html: html}} />
         </Layout>
     );
+
+
+    // return (
+    //     <Layout>
+    //         <code>
+    //             <pre>{JSON.stringify(props, null, 4)}</pre>
+    //         </code>
+    //     </Layout>
+    // );
 });
 
 PostTemplate.displayName = 'PostTemplate';
